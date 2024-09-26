8839fd65e7bf92b74103d7ce2fc4aa1d4347dd98
tf7software <160179762+tf7software@users.noreply.github.com>
2024-09-26 13:24:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+// Serve homepage
+app.get('/snake', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/snake.html'));
+});
+
 // In-memory cache for search results
 const searchCache = new Map();
 
