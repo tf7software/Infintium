cdebca88e7cb865dcd388a09b7a998b6b48bfac0
tf7software <160179762+tf7software@users.noreply.github.com>
2024-09-27 13:12:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.get('/pacman', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/pacman.html'));
 });
 
+app.get('/view', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/view.html'));
+});
+
+
 
 // Serve homepage
 app.get('/snake', (req, res) => {
